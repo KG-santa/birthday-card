3c44e06e52aa737aa90aa0418f3c5b2852d0b05f
Anastasia Kolesnyk <ana.ko8915@gmail.com>
2025-03-27 13:00:26 +0200

    header animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import "../styles/Header.scss";
 
 interface HeaderProps {
@@ -14,6 +14,32 @@ const Header: React.FC<HeaderProps> = ({
   isTextFirst = false,
   className,
 }) => {
+  const [isZooming, setIsZooming] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false); // Track if animation is active
+
+  const handleImageClick = () => {
+    if (isAnimating) return; // Ignore click if animation is active
+
+    const repeatCount = 3; // Number of times the animation should repeat
+    let count = 0;
+
+    setIsAnimating(true); // Mark animation as active
+
+    const intervalId = setInterval(() => {
+      setIsZooming(true);
+
+      setTimeout(() => {
+        setIsZooming(false);
+      }, 900); // Slightly less than the animation duration (1s)
+
+      count++;
+      if (count >= repeatCount) {
+        clearInterval(intervalId); // Stop the interval after the desired count
+        setIsAnimating(false); // Mark animation as inactive
+      }
+    }, 1000); // Matches the animation duration
+  };
+
   return (
     <header className={`header ${className}`}>
       {isTextFirst && title && (
@@ -21,7 +47,12 @@ const Header: React.FC<HeaderProps> = ({
           {title}
         </h1>
       )}
-      <img src={imageUrl} alt="Header Image" className="header-image" />
+      <img
+        src={imageUrl}
+        alt="Header Image"
+        className={`header-image ${isZooming ? "zooming" : ""}`}
+        onClick={handleImageClick}
+      />
       {!isTextFirst && title && (
         <h1 className="header-title" data-text={title}>
           {title}
